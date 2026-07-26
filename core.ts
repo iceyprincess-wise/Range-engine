be03ccf55c9156fff2ca294257586037ac19ac48
iceyprincess-wise <demmyfagboo@gmail.com>
2026-07-26 01:20:25 +0100

    chore: remove stray core.ts, orphaned src/, and broken Range-Engine-Logic submodule pointer

--- core.ts
+++
@@ -1,7 +0,0 @@
-export * from "./format";
-export * from "./leagueDna";
-export * from "./teamDb";
-export * from "./types";
-export * from "./storage";
-export * from "./research";
-export * from "./runEngine";
